d4d0d1a1bca45d8ad51afc205b0af805fef2f75f
João Manoel <joaomanoel7@gmail.com>
2014-11-30 00:00:21 -0200

    add useful firefox scripts

--- bin/firefox-config.sh
+++ bin/firefox-config.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-sudo update-alternatives --install "/usr/bin/gnome-www-browser" "gnome-www-browser" /opt/firefox/firefox 50
-sudo update-alternatives --install "/usr/bin/x-www-browser"     "x-www-browser"     /opt/firefox/firefox 50
+sudo update-alternatives --install "/usr/bin/gnome-www-browser" "gnome-www-browser" ~/.local/firefox/firefox 100
+sudo update-alternatives --set     "gnome-www-browser"                              ~/.local/firefox/firefox
+sudo update-alternatives --install "/usr/bin/x-www-browser"     "x-www-browser"     ~/.local/firefox/firefox 100
+sudo update-alternatives --set     "x-www-browser"                                  ~/.local/firefox/firefox
