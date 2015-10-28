5f8b248d1b96335de2b62456a640757ae31e3870
João Manoel <joaomanoel7@gmail.com>
2015-10-28 18:54:36 -0200

    acesso ao qcademico and visao via xfreerdp

---
+++ bin/acesso-qacademico.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec xfreerdp /flag +window-drag +clipboard /u:2219692 /v:200.17.56.204 /size:$1 /drive:home,/home/user
---
+++ bin/acesso-visao.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+exec xfreerdp /flag +window-drag +clipboard /u:2219692 /v:10.1.231.234 /size:$1 /drive:home,/home/user
