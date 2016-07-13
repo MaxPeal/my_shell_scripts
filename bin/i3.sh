6a5b7dc9aa30b4d5e0183281236257c5171249ab
João Manoel <joaomanoel7@gmail.com>
2016-07-12 23:28:52 -0300

    update repo

--- bin/i3.sh
+++ bin/i3.sh
@@ -4,20 +4,20 @@ if [ -f ~/.xsessionrc ]; then
     . ~/.xsessionrc
 fi
 
-(sleep 1; killall -9 gnome-keyring-daemon >/dev/null 2>&1) &
+#(sleep 0.5; gnome-keyring-daemon --components=ssh,secrets,pkcs11) &
+(sleep 0.5; xsetroot -solid '#2E3436') &
+#(sleep 0.5; xsetbg -fullscreeb -onroot ~/.background) &
+(sleep 0.5; compton -b -f -m 0.95) &
 (sleep 1; thunar --daemon) &
 (sleep 1; nm-applet) &
-(sleep 2; xsetroot -solid '#2E3436') &
-#(sleep 2; nitrogen --restore) &
-(sleep 2; compton -b -f -m 0.95) &
-#(sleep 3; volti) &
-(sleep 3; volumeicon) &
-(sleep 3; clipit) &
-(sleep 4; dropbox start -i) &
-#(sleep 4; pamac-tray) &
+#(sleep 2; volti) &
+(sleep 2; volumeicon) &
+(sleep 2; clipit) &
+(sleep 3; dropbox start -i) &
+#(sleep 3; pamac-tray) &
 #(sleep 4; hp-systray -x) &
 #(sleep 4; /opt/teamviewer/tv_bin/script/teamviewer) &
-(sleep 4; conkywonky.sh) &
+(sleep 5; conkywonky.sh) &
 (sleep 5; i3-sensible-terminal) &
 (sleep 5; killall -9 gnome-keyring-daemon >/dev/null 2>&1) &
 
