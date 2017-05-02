fa78d32eb2e25c5d3f0b823138cc75885aca96b2
João Manoel <joaomanoel7@gmail.com>
2017-05-02 02:01:38 -0300

    update repo ...

--- bin/faraday-start.sh
+++ bin/faraday-start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo invoke-rc.d couchdb start
 cd ~/src/security/faraday
+./faraday-server.py &
 exec ./faraday.py
--- bin/faraday-stop.sh
+++ bin/faraday-stop.sh
@@ -4,3 +4,6 @@ sudo killall -9 couchdb
 sudo killall -9 beam.smp
 pid_faraday="$(ps axw | grep faraday.py | grep -v grep | cut -d ' ' -f 1)"
 [ -n "$pid_faraday" ] && sudo kill -9 "$pid_faraday"
+pid_faraday_server="$(ps axw | grep faraday-server.py | grep -v grep | cut -d ' ' -f 1)"
+[ -n "$pid_faraday_server" ] && sudo kill -9 "$pid_faraday_server"
+exit 0
